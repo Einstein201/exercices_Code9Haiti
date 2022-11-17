7dc5bad61e734e1a13a29c97297d3276d1eddab0
Einstein <84164381+Einstein201@users.noreply.github.com>
2022-11-17 00:48:31 -0500

    Add files via upload

--- exo11.js
+++ exo11.js
@@ -1,11 +1,29 @@
 
 						// Enonse #11
 						// Nan yon tablo nonb, 
-						// kreye pwòp fason pa w pou w afiche sa ki pi gran, ak sa ki piti a.
+			// kreye pwòp fason pa w pou w afiche sa ki pi gran, ak sa ki piti a.
 
-let tab=[110,22,31,4,40,5,6,55,76,21,12,7,999,0]
-let x=Math.min(...tab)    
-let y = Math.max(...tab)  
+   let myArray = [100, 56, 6, 32, 53,130, 516, 26, 332, 53];						
 
-console.log("Vale maximal: " + y);
-console.log("Vale minimum: " + x);
+	let max=myArray[0], min=myArray[0];
+
+	for(let i=0; i<myArray.length; i++){
+	    if(myArray[i]<min){
+	        min=myArray[i]
+	    }
+	    
+	}
+
+	console.log("Pi piti a se: "+ min)
+
+	for(let j=0; j<myArray.length; j++){
+	    
+	    if(myArray[j]>max){
+	        max=myArray[j]
+	    }
+	}
+
+
+	console.log("Pi gran nonb lan se: " + max)
+
+
